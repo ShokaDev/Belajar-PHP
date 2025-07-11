884acc1ca4f9de0ad68e2a5a9cab94c45836cbb6
Faharel <Fahareladitthapratama@gmail.com>
2025-07-11 18:42:39 +0700

    docs(learning): log today's PHP learning progress via YouTube
    - Learned basic PHP concepts from a YouTube video (light coding + explanations)
    - Practiced writing a simple "Hello World" script
    - Studied and tested data types: integer, float, boolean, and string
    - Focused more on understanding concepts; minimal hands-on coding

---
+++ TipeDataNumber.php
@@ -0,0 +1,31 @@
+<?php 
+
+echo "Decimal : "; // [Base 10]
+var_dump(1234); // Output : int(1234)
+
+echo "Octal : "; // [Base 8]
+var_dump(0123); // Output : int(83)
+
+echo "Hexadecimal : "; // [Base 16]
+var_dump(0x1A); // Output : int(26)
+
+echo "Binary : "; // [Base 2]
+var_dump(0b11111111); // Output : int(255)
+
+echo "Underscore di Number : ";
+var_dump(1_234_567); // Output : int(1234567)
+
+
+// Floating Point
+
+echo "Floating Point : ";
+var_dump(1.234); // Output : float(1.234)
+
+echo "Floating Point dengan E notation (1.2 x 1000)  : ";
+var_dump(1.2e3); // Output : float(1200)
+
+echo "Floating Point dengan E notation Minus (7 x 0.001) : ";
+var_dump(7e-3); // Output : float(0.007)
+
+echo "Undescore di Floating Point : ";
+var_dump(1_234.567); // Output : float(1234.567)
---
+++ TipeDataString.php
@@ -0,0 +1,8 @@
+<?php
+// Kalau echo menggunakan kutip 1 maka hasilnya akan menampilkan String
+echo 'Nama Kamu : ';
+echo 'Faharel Adittha Pratama';
+echo "\n";
+
+echo 'Nama Kamu : ';
+echo "Faharel\t Adittha\t Pratama\n";
