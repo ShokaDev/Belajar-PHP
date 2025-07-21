9e854d485d0e0f4c57daf739dc521724bf283a54
Faharel <Fahareladitthapratama@gmail.com>
2025-07-21 20:12:14 +0700

    docs: study heredoc, nowdoc, and variable theory in PHP

--- TipeDataString.php
+++ TipeDataString.php
@@ -3,6 +3,32 @@
 echo 'Nama Kamu : ';
 echo 'Faharel Adittha Pratama';
 echo "\n";
+// echo "\n"; ini berguna sama seperti tag <br> pada html
 
-echo 'Nama Kamu : ';
+echo "Nama Kamu : ";
 echo "Faharel\t Adittha\t Pratama\n";
+// Sementara \t berguna untuk membuat jarak antar teks, jadi tiap ada \t akan membuat jarak setelahnya
+
+
+
+// Nama materi dibawah ini Heredoc, berguna untuk menampilkan banyak teks dengan "Enter" didalamnya tanpa menggunakan \n
+// Heredoc
+echo <<<Farel
+Ini kita yapping aja ya disini
+karena aku gatau mau ngetik apa
+intinya panjang aja karena mau ngetes
+tentang materi Herodoc
+Farel;
+
+
+// Nowdoc
+echo <<<'Farel'
+Ini kita yapping aja ya disini
+karena aku gatau mau ngetik apa
+intinya panjang aja karena mau ngetes
+tentang materi Nowdoc
+Farel;
+
+// Mungkin disini kurang keliatan perbedaan nya
+// tapi intinya perbedaan antar Herodoc dan Nowdoc itu, kalau Herodoc bisa membaca variabel, jadi kita bisa menggunakan variabel seperti $Nama dan \n & \t.
+// Sementara untuk Nowdoc dia bener bener menampilkan semua keseluruhan teks nya tanpa membaca isi variabel @Nama, dan tidak bisa menggunakan \n & \t
